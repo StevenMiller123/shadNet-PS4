dd1cd6466573f1468052e721be1f286b14fa633e
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-08-30 02:13:00 -0500

    Retrieve game serial

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -15,8 +15,18 @@ extern "C" s32 client_start() {
     // Initialize elfinfo
     auto& game_info = Common::ElfInfo::Instance();
 
+    // Retrieve as much game metadata as we can
+    OrbisAppInfo app_info{};
+    s32 result = sceKernelGetAppInfo(getpid(), &app_info);
+    if (result != 0) {
+        LOG_ERROR("sceKernelGetAppInfo failed!");
+        return 1;
+    }
+
+    LOG_INFO("Currently running {}", app_info.TitleId);
     sceKernelGetCompiledSdkVersion(reinterpret_cast<s32*>(&game_info.sdk_ver));
     game_info.initialized = true;
+    game_info.game_serial = std::string{app_info.TitleId};
 
     // Initialize NpHandler
     Libraries::Np::NpHandler::GetInstance().Initialize();
