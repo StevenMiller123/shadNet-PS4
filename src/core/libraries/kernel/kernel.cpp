36d5111aad38abc89bd4b6070aece23979d681b7
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-09-03 20:09:30 -0500

    Hook mmap and sceKernelMmap to force MAP_SYSTEM
    Hooks are consuming flex budget through anon mmaps and mallocs. Force MAP_SYSTEM for these instead to bypass this as much as possible.

--- src/core/libraries/kernel/kernel.cpp
+++ src/core/libraries/kernel/kernel.cpp
@@ -2,10 +2,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <sys/socket.h>
+#include <sys/mman.h>
 
 #include "common/logging/log.h"
 #include "core/libraries/kernel/kernel.h"
 
+HOOK_INIT(mmap);
+HOOK_INIT(sceKernelMmap);
 HOOK_INIT(socket);
 HOOK_INIT(__sys_socketex);
 
@@ -19,7 +22,25 @@ s32 __sys_socketex_hook(const char* name, s32 family, s32 type, s32 protocol) {
     return HOOK_CONTINUE(__sys_socketex, s32 (*)(const char*, s32, s32, s32), name, family, type, protocol);
 }
 
+void* mmap_hook(void* addr, u64 len, s32 prot, s32 flags, s32 fd, s64 offset) {
+    if ((flags & 0x1000) == 0x1000 && (flags & 0x2000) == 0) {
+        LOG_INFO("called with MAP_ANON, appending MAP_SYSTEM");
+        flags |= 0x2000;
+    }
+    return HOOK_CONTINUE(mmap, void* (*)(void*, u64, s32, s32, s32, s64), addr, len, prot, flags, fd, offset);
+}
+
+s32 sceKernelMmap_hook(void* addr, u64 len, s32 prot, s32 flags, s32 fd, s64 offset, void** result) {
+    if ((flags & 0x1000) == 0x1000 && (flags & 0x2000) == 0) {
+        LOG_INFO("called with MAP_ANON, appending MAP_SYSTEM");
+        flags |= 0x2000;
+    }
+    return HOOK_CONTINUE(sceKernelMmap, s32 (*)(void*, u64, s32, s32, s32, s64, void**), addr, len, prot, flags, fd, offset, result);
+}
+
 void RegisterKernelHooks() {
+    HOOK(mmap);
+    HOOK(sceKernelMmap);
     HOOK(socket);
     HOOK(__sys_socketex);
 }
