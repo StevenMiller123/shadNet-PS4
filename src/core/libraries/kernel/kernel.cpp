f0d8350f668372512d249f589bfcd7b02189d6b0
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-09-07 15:24:36 -0500

    Remove mmap hooks
    Apparently non of us actually made sure they worked on real hardware. Go figure, they didn't.
    I guess there's some weird interaction going on when trying to patch code from the plugins sdk, so I'll need to fork the plugin sdk and make it do mmap with MAP_SYSTEM instead of forcing MAP_SYSTEM through hooks.

--- src/core/libraries/kernel/kernel.cpp
+++ src/core/libraries/kernel/kernel.cpp
@@ -7,24 +7,6 @@
 #include "common/logging/log.h"
 #include "core/libraries/kernel/kernel.h"
 
-HOOK_INIT(sceKernelMmap);
-s32 sceKernelMmap_hook(void* addr, u64 len, s32 prot, s32 flags, s32 fd, s64 offset, void** result) {
-    if ((flags & 0x1000) == 0x1000 && (flags & 0x2000) == 0) {
-        // Append MAP_SYSTEM, that way mmaps used to create hooks aren't consuming flex budget.
-        flags |= 0x2000;
-    }
-    return SHADNET_HOOK_CONTINUE(sceKernelMmap, addr, len, prot, flags, fd, offset, result);
-}
-
-HOOK_INIT(mmap);
-void* mmap_hook(void* addr, u64 len, s32 prot, s32 flags, s32 fd, s64 offset) {
-    if ((flags & 0x1000) == 0x1000 && (flags & 0x2000) == 0) {
-        // Append MAP_SYSTEM, that way mmaps used to create hooks aren't consuming flex budget.
-        flags |= 0x2000;
-    }
-    return SHADNET_HOOK_CONTINUE(mmap, addr, len, prot, flags, fd, offset);
-}
-
 HOOK_INIT(socket);
 s32 socket_hook(s32 family, s32 type, s32 protocol) {
     LOG_INFO(Lib_Kernel, "called, family = {}, type = {}, protocol = {}", family, type, protocol);
@@ -38,8 +20,6 @@ s32 __sys_socketex_hook(const char* name, s32 family, s32 type, s32 protocol) {
 }
 
 void RegisterKernelHooks() {
-    HOOK(sceKernelMmap);
-    HOOK(mmap);
     HOOK(socket);
     HOOK(__sys_socketex);
 }
